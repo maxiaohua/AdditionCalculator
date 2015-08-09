a54cdd01d8848c2dffda79dbdaffba2858dfb08a
心忆年华 <mazhihua5152570@126.com>
2015-08-09 16:13:19 +0800

    添加触摸方法

--- AdditionCalculator/AdditionCalculator/ViewController.m
+++ AdditionCalculator/AdditionCalculator/ViewController.m
@@ -19,6 +19,11 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    NSLog(@"aaa");
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
